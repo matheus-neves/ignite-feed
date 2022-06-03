038816fbdb0320aebab57b1410fbf50142fe947d
Matheus Neves <matheusneves1989@gmail.com>
2022-06-03 16:02:46 -0300

    feat(Sidebar): add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { Header } from "./components/Header";
 import './styles/global.css';
 
 import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <aside>
-          <h1>Sidebar</h1>
-        </aside>
+        <Sidebar />
         <main>
           <h1>Post</h1>
         </main>
