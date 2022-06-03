b00e104ff966fe65748612bdd0233f5ab96e91df
Matheus Neves <matheusneves1989@gmail.com>
2022-06-03 15:17:43 -0300

    feat(App): create wrapper styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import { Header } from "./components/Header";
 import './styles/global.css';
 
+import styles from './App.module.css'
+
 export function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <aside>
+          <h1>Sidebar</h1>
+        </aside>
+        <main>
+          <h1>Post</h1>
+        </main>
+      </div>
+    </>
+  )
 }
